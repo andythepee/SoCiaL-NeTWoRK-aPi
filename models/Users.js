5361a12782c7bafb282fa8be07127e99aa133bec
Andrew <andy.p.123@hotmail.com>
2023-07-02 13:39:52 -0400

    Updated Models

---
+++ models/Users.js
@@ -0,0 +1,49 @@
+// Require Mongoose
+const { Schema, model } = require("mongoose");
+
+const UsersSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thoughts",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Users",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+// Total Friend Count
+UsersSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+// Create User model using the User Schema
+const Users = model("Users", UsersSchema);
+
+// Export Users module
+module.exports = Users;
